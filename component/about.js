c9a66fb57a9808ae9394d33373e701f19613c40d
Techiguru <123058835+mainguruhoon@users.noreply.github.com>
2025-05-19 23:42:28 +0530

    Add files via upload

---
+++ component/about.js
@@ -0,0 +1,94 @@
+import React from 'react'
+
+
+function about() {
+  return (
+    <div>
+        <div>
+            <h1>About Us</h1>
+            <p>Welcome to INSTITUTEMANAGEMENT We are a leading provider of innovative solutions designed to transform your business. Our team is dedicated to delivering excellence in every project we undertake.</p>
+            <h3>Our Values</h3>
+            <li>
+                <ul>Innovation</ul>
+                <ul>Integrity</ul>
+                <ul>Customer Focus</ul>
+                <ul>Excellence</ul>
+            
+
+                
+            </li>
+            <h3>Our Mission</h3>
+            <p>Our mission is to empower businesses with cutting-edge technology and solutions that drive success. We aim to build long-lasting relationships with our clients based on trust, professionalism, and mutual growth.</p>
+        </div>
+        <h1 id='a'>
+            Story From Our User
+        </h1>
+        <p>This story is tell by users</p>
+
+        <div id='col-3'>
+        <div className="card" style="width: 18rem;">
+  <img src="..." className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">Card title</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+
+    <div className="card" style="width: 18rem;">
+  <img src="..." className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">Card title</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+
+    <div className="card" style="width: 18rem;">
+  <img src="..." className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">Card title</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <div className="card" style="width: 18rem;">
+  <img src="..." className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">Card title</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    
+  </div>
+</div>
+    
+  </div>
+</div>
+  
+  </div>
+</div>
+    
+  </div>
+</div>
+
+        </div>
+        <div>
+            <h1 id='b'>What Other Users Say</h1>
+
+            <div id="c" className="carousel slide">
+  <div className="carousel-inner">
+    <div className="carousel-item active">
+      <img src="..." className="d-block w-100" alt="..."/>
+    </div>
+    <div className="carousel-item">
+      <img src="..." className="d-block w-100" alt="..."/>
+    </div>
+    <div className="carousel-item">
+      <img src="..." className="d-block w-100" alt="..."/>
+    </div>
+  </div>
+  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Previous</span>
+  </button>
+  <button className="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Next</span>
+  </button>
+</div>
+        </div>
+    </div>
+  )
+}
+
+export default about
